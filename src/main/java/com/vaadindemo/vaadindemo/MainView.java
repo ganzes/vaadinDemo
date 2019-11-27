3f1ebd04714b60aeb53e41849881c6d09d369af8
ganzes <charczuk.arkadiusz@gmail.com>
2019-11-27 20:39:27 +0100

    added nodejs

---
+++ src/main/java/com/vaadindemo/vaadindemo/MainView.java
@@ -0,0 +1,14 @@
+package com.vaadindemo.vaadindemo;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Button("Click me!", event -> Notification.show("Hello Workd")));
+    }
+}
