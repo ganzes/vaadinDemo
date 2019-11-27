eaa3973071d438fa4bc0973e2ae0b4c90b9c8d74
ganzes <charczuk.arkadiusz@gmail.com>
2019-11-27 19:46:52 +0100

    initial commit

---
+++ src/test/java/com/vaadindemo/vaadindemo/VaadindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaadindemo.vaadindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadindemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
